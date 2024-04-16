4df22cff8a9b7eeae7ee3412fc36bcc08eaa6041
Raghav agarwal <raghavagarwal3131@gmail.com>
2024-04-16 15:18:51 +0530

    Final touch

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'EventVerse',
+  description: 'Eventverse is a platform for event management.',
   icons: {
     icon: '/assets/images/logo.svg'
   }
